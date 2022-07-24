395c6aa027d8ca468197de705b66abd48a6668c9
JureSeselj <jure.seselj@gmail.com>
2022-07-24 10:53:19 +0000

    Add end.js file

---
+++ js/end.js
@@ -0,0 +1,30 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+window.onload = document.body.style.backgroundImage = "url('assets/images/highscore-image.jpg')";
+
+finalScore.innerText = 'Score:' + ' ' + mostRecentScore;
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
+
+let saveHighScore; 
+
+saveHighScore = (e) => {
+    e.preventDefault();
+        
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('highscores.html');
+};
