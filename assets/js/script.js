965ea9379a94d3ef1c676df1f4f72026949fe3f6
JureSeselj <jure.seselj@gmail.com>
2022-07-31 08:11:39 +0000

    Add function to initialize page for new question

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ let currentQuestion;
 let shuffleQuestions;
 
 // Begin Game 
+
 let scorePoints = 0;
 window.onload = function beginGame() {
     background();
@@ -16,12 +17,14 @@ window.onload = function beginGame() {
 };
 
 // Next Button Add Next Question
+
 nextButton.addEventListener('click', () => {
     currentQuestion++;
     nextQuestion();
 });
 
 // When Executed Hide Start Button, Shows Question Container And Shuffle Questions 
+
 function startQuiz() {
     shuffleQuestions = questionBank.sort(() => Math.random() - 0.5);
     currentQuestion = 0;
