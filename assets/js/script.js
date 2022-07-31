b927f7912bafea58230660ed2c7c61d1625efad7
JureSeselj <jure.seselj@gmail.com>
2022-07-31 10:32:57 +0000

    Add 3 questions with answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,36 +5,39 @@ const questionPopupElement = document.getElementById('popup-incorrect');
 const progressText = document.getElementById("progress-text");
 const progressBarFull = document.getElementById("progressBarFull");
 
+
+
 let currentQuestion;
 let shuffleQuestions;
 
-// Begin Game 
-
+// begin game 
 let scorePoints = 0;
 window.onload = function beginGame() {
     background();
     startQuiz();
 };
 
-// Next Button Add Next Question
+//add background to body
+function background() {
+    document.body.style.backgroundImage = "url('assets/images/highscore-image.jpg')";
+}
 
+// next button add next question
 nextButton.addEventListener('click', () => {
     currentQuestion++;
     nextQuestion();
 });
 
-// When Executed Hide Start Button, Shows Question Container And Shuffle Questions 
-
+// When executed hide Start Button, shows Question container and Shuffle questions. 
 function startQuiz() {
     shuffleQuestions = questionBank.sort(() => Math.random() - 0.5);
     currentQuestion = 0;
     nextQuestion();
 }
 
-const MAX_QUESTIONS = 20;
+const MAX_QUESTIONS = 10;
 
-// Initialize Page For New Question
-
+// initialize page for new question.
 function nextQuestion() {
 
     if ((shuffleQuestions.length == currentQuestion) || currentQuestion == MAX_QUESTIONS) {
@@ -48,68 +51,65 @@ function nextQuestion() {
         myTimer();
     }
 
-    // Update The Progress Text
+    // update the progress text
     progressText.innerText = `Question ${currentQuestion+1}/${MAX_QUESTIONS}`;
-    // Update The Progress Bar
+    //Update the progress bar
     progressBarFull.style.width = `${((currentQuestion +1) / MAX_QUESTIONS) * 100}%`;
 }
 
-//Receive Question With Answers And Outputs Buttons For Each Answer
+function initMap() {}
 
+//Receive question with answers and outputs buttons for each answer
 function showQuestion(pQuestion) {
     questionElement.innerText = pQuestion.question;
     questionPopupElement.innerText = pQuestion.correctText;
     const questionImage = document.getElementById("question-image");
     questionImage.src = pQuestion.image;
-};
 
-// Create Buttons To Answers And Add Text From Question Bank
+    // create buttons to answers and add text from question bank
+    pQuestion.answers.forEach(answer => {
+        const answerButton = document.createElement('button');
+        answerButton.innerText = answer.text;
+        answerButton.classList.add('button');
+        choiceButtons.appendChild(answerButton);
 
-pQuestion.answers.forEach(answer => {
-    const answerButton = document.createElement('button');
-    answerButton.innerText = answer.text;
-    answerButton.classList.add('button');
-    choiceButtons.appendChild(answerButton);
+        if (answer.correct) {
+            answerButton.dataset.correct = answer.correct;
+        }
+        answerButton.addEventListener('click', selectedAnswer);
+    });
+}
 
-    if (answer.correct) {
-        answerButton.dataset.correct = answer.correct;
-    }
-    answerButton.addEventListener('click', selectedAnswer);
-});
-
-// Add Hide On Next Button And Clean Buttons From Previous Question
-
+// Add hide on next button and clean buttons from previous question
 function resetQuestion() {
     while (choiceButtons.firstChild) {
         choiceButtons.removeChild(choiceButtons.firstChild);
     }
-};
+}
 
-// Target On Selected Answer And Style It Correct Or Wrong 
-
+// target on selected asnwer and style it correct or wrong 
 function selectedAnswer(a) {
     const selectedButton = a.target;
     const correct = selectedButton.dataset.correct;
     clearInterval(timer);
 
-    // Add Class If Correct
+    // add class if correct
     if (correct) {
         this.classList.add('correct');
-        // Go To nextQuestion()
+        //go to nextQuestion()
         scorePoints++;
-        // Add Class If Incorrect
+        // add class if incorrect
     } else {
         this.classList.add('wrong');
         wrongAnswer();
     }
-    // Disable Answer Buttons
+    // disable answer buttons
     Array.from(choiceButtons.children).forEach(button => {
         button.disabled = true;
     });
-};
+}
 
-// Show Correct Answer After Wrong Click
-
+// Show correct answer after wrong click
 function wrongAnswer() {
     var modal = document.getElementById('myModal');
     var span = document.getElementsByClassName('close')[0];
@@ -123,10 +123,9 @@ function wrongAnswer() {
             modal.style.display = "none";
         }
     };
-};
+}
 
-// Add Countdown Timer
-
+// Add contdown timer
 let timer;
 
 function myTimer() {
@@ -145,31 +144,78 @@ function myTimer() {
             });
         }
     }, 1000);
-};
+}
 
 // Question Bank
 
 const questionBank = [{
     question: "I'll visit a famous volcano that detroyed 5 cities in AD 79 and until nowadays is one of the most dangerous in the world.",
-        answers: [{
-                text: 'Mount Samalas - Indonesia',
-                correct: false
-            },
-            {
-                text: 'Mount Vesuvius - Italy',
-                correct: true
-            },
-            {
-                text: 'Mount Unzen - Japan',
-                correct: false
-            }, {
-                text: 'Santorini - Greece',
-                correct: false
-            }
-        ],
-        correctText: "The Sahara is a desert on the African continent. With an area of 9,200,000 square kilometres (3,600,000 sq mi), it is the largest hot desert in the world and the third largest desert overall, smaller only than the deserts of Antarctica and the northern Arctic.The desert comprises much of North Africa, excluding the fertile region on the Mediterranean Sea coast, the Atlas Mountains of the Maghreb, and the Nile Valley in Egypt and Sudan. It stretches from the Red Sea in the east and the Mediterranean in the north to the Atlantic Ocean in the west, where the landscape gradually changes from desert to coastal plains. To the south, it is bounded by the Sahel, a belt of semi-arid tropical savanna around the Niger River valley and the Sudan Region of Sub-Saharan Africa. The Sahara can be divided into several regions, including the western Sahara, the central Ahaggar Mountains, the Tibesti Mountains, the Aïr Mountains, the Ténéré desert, and the Libyan Desert. ",
-        image: 'assets/images/saharadesert.jpg',
-        mark: "Sahara desert"
-    },
-];
+    answers: [{
+            text: 'Mount Samalas - Indonesia',
+            correct: false
+        },
+        {
+            text: 'Mount Vesuvius - Italy',
+            correct: true
+        },
+        {
+            text: 'Mount Unzen - Japan',
+            correct: false
+        }, {
+            text: 'Santorini - Greece',
+            correct: false
+        }
+    ],
+    correctText: 'The eruption of Mount Vesuvius in AD 79 destroyed the Roman cities of Pompeii, Herculaneum, Oplontis and Stabiae, as well as several other settlements. Today, it is regarded as one of the most dangerous volcanoes in the world because of the population of 3,000,000 people living near enough to be affected by an eruption, with 600,000 in the danger zone, making it the most densely populated volcanic region in the world.',
+    image: 'assets/images/vesuvius.jpg',
+    mark: "Mount Vesuvius"
+},
 
+{
+    question: "Dracula famously lived in the historical region of Transylvania – but in what country would I'll find his castle?",
+    answers: [{
+            text: 'Hungary',
+            correct: false
+        },
+        {
+            text: 'Romania',
+            correct: true
+        },
+        {
+            text: 'Slovakia',
+            correct: false
+        }, {
+            text: 'Czech Republic',
+            correct: false
+        }
+    ],
+    correctText: "Bran Castle is a castle in Bran.  It is a national monument and landmark in Transylvania. Commonly known outside Transylvania as Dracula's Castle, it is often referred to as the home of the title character in Bram Stoker's Dracula. There is no evidence that Stoker knew anything about this castle, which has only tangential associations with Vlad the Impaler, voivode of Wallachia, the putative inspiration for Dracula. Stoker's description of Dracula's crumbling fictional castle also bears no resemblance to Bran Castle.",
+    image: 'assets/images/bran-castle.jpg',
+    mark: "Bran Castle"
+
+},
+
+{
+    question: "I'll finally visit the city where Antonio Gaudi's Holy Family Basilica is located! This church is astonishing!",
+    answers: [{
+            text: 'Madrid',
+            correct: false
+        },
+        {
+            text: 'Sevilha',
+            correct: false
+        },
+        {
+            text: 'Bilbau',
+            correct: false
+        }, {
+            text: 'Barcelona',
+            correct: true
+        }
+    ],
+    correctText: "The Basílica de la Sagrada Família ('Basilica of the Holy Family'), also known as the Sagrada Família, is a large unfinished Roman Catholic minor basilica in the Eixample district of Barcelona, Catalonia, Spain. Designed by the Spanish architect Antoni Gaudí (1852–1926), his work on the building is part of a UNESCO World Heritage Site.",
+    image: 'assets/images/holy-family.jpg',
+    mark: "The Basílica de la Sagrada Família"
+},
+
+];
